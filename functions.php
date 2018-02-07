e1a62fc34acbb3e8dc1c40e6a10bc44d3f17c779
Jorge Saud <jorgelsaud@gmail.com>
2018-02-07 11:46:28 -0300

    add header options page

--- functions.php
+++ functions.php
@@ -11,33 +11,33 @@ function avada_lang_setup() {
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
 
-if( function_exists('acf_add_options_page') ) {
+// if( function_exists('acf_add_options_page') ) {
 	
-	acf_add_options_page(array(
-		'page_title'=>'Mercadeo Options',
-		'menu_title' => '',
-		'menu_slug' => 'mercadeo_options',
-		'capability' => 'edit_posts',
-		'position' => false,
-		'parent_slug' => '',
-		'icon_url' => false,
-		'redirect' => true,
-		'post_id' => 'mercadeo_options',
-		'autoload' => false
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Header Settings',
-		'menu_title' 	=> 'Header Settings',
-		'menu_slug'=>'header_mercadeo',
-		'capability' => 'edit_posts',
-		'parent_slug' => 'mercadeo_options',
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Footer Settings',
-		'menu_title' 	=> 'Footer Settings',
-		'menu_slug'=>'footer_mercadeo',
-		'capability' => 'edit_posts',
-		'parent_slug' => 'mercadeo_options',
-	));
+// 	acf_add_options_page(array(
+// 		'page_title'=>'Mercadeo Options',
+// 		'menu_title' => '',
+// 		'menu_slug' => 'mercadeo_options',
+// 		'capability' => 'edit_posts',
+// 		'position' => false,
+// 		'parent_slug' => '',
+// 		'icon_url' => false,
+// 		'redirect' => true,
+// 		'post_id' => 'mercadeo_options',
+// 		'autoload' => false
+// 	));
+// 	acf_add_options_sub_page(array(
+// 		'page_title' 	=> 'Header Settings',
+// 		'menu_title' 	=> 'Header Settings',
+// 		'menu_slug'=>'header_mercadeo',
+// 		'capability' => 'edit_posts',
+// 		'parent_slug' => 'mercadeo_options',
+// 	));
+// 	acf_add_options_sub_page(array(
+// 		'page_title' 	=> 'Footer Settings',
+// 		'menu_title' 	=> 'Footer Settings',
+// 		'menu_slug'=>'footer_mercadeo',
+// 		'capability' => 'edit_posts',
+// 		'parent_slug' => 'mercadeo_options',
+// 	));
 	
-}
+// }

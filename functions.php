c61aa1301ca5c14dec869efcc9c4a521c1b15507
Jorge Saud <jorgelsaud@gmail.com>
2018-02-07 11:25:13 -0300

    add header options page

--- functions.php
+++ functions.php
@@ -13,7 +13,23 @@ add_action( 'after_setup_theme', 'avada_lang_setup' );
 
 if( function_exists('acf_add_options_page') ) {
 	
-	acf_add_options_page();
-	acf_add_options_sub_page('Header');
+	acf_add_options_page(array(
+		'page_title'=>'Mercadeo Options',
+		'menu_title' => '',
+		'menu_slug' => 'mercadeo_options',
+		'capability' => 'edit_posts',
+		'position' => false,
+		'parent_slug' => '',
+		'icon_url' => false,
+		'redirect' => true,
+		'post_id' => 'mercadeo_options',
+		'autoload' => false
+	));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Header Settings',
+		'menu_title' 	=> 'Header Settings',
+		'redirect' 		=> false,
+		'parent_slug' => 'mercadeo_options',
+	));
 	
 }

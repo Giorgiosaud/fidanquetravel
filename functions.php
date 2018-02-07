a22fc84de4c81d34707b88ffe101f0f745d8a913
Jorge Saud <jorgelsaud@gmail.com>
2018-02-07 11:13:12 -0300

    add header options page

--- functions.php
+++ functions.php
@@ -10,3 +10,10 @@ function avada_lang_setup() {
 	load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	acf_add_options_sub_page('Header');
+	
+}

43274c1ca0b2f73cd11ade920ff77f2dd380e628
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 07:25:31 -0300

    paquetes fix support

--- functions.php
+++ functions.php
@@ -11,66 +11,11 @@ function avada_lang_setup() {
 	load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
+define('CMB2PREFIX','fidanquet_');
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
 require 'optionspage.php';
 require 'inc/paquetes.php';
-//ACF Package
-add_filter('acf/settings/path', 'my_acf_settings_path');
-function my_acf_settings_path( $path ) {
- 
-    // update path
-    $path = get_stylesheet_directory() . '/acf/';
-    
-    // return
-    return $path;
-    
-}
-// 2. customize ACF dir
-add_filter('acf/settings/dir', 'my_acf_settings_dir');
- 
-function my_acf_settings_dir( $dir ) {
- 
-    // update path
-    $dir = get_stylesheet_directory_uri() . '/acf/';
-    
-    // return
-    return $dir;
-    
-}
-
-// 3. Hide ACF field group menu item
-// add_filter('acf/settings/show_admin', '__return_false');
-
-
-// 4. Include ACF
-include_once( get_stylesheet_directory() . '/acf/acf.php' );
-
-// if( function_exists('acf_add_options_page') ) {
-	
-// 	acf_add_options_page(array(
-// 		'page_title'=>'Mercadeo Options',
-// 		'menu_title' => '',
-// 		'menu_slug' => 'mercadeo_options',
-// 		'capability' => 'edit_posts',
-// 		'position' => false,
-// 		'parent_slug' => '',
-// 		'icon_url' => false,
-// 		'redirect' => true,
-// 		'post_id' => 'mercadeo_options',
-// 		'autoload' => false
-// 	));
-// 	acf_add_options_sub_page(array(
-// 		'page_title' 	=> 'Header Settings',
-// 		'menu_title' 	=> 'Header Settings',
-// 		'menu_slug'=>'header_mercadeo',
-// 		'capability' => 'edit_posts',
-// 		'parent_slug' => 'mercadeo_options',
-// 	));
-// 	acf_add_options_sub_page(array(
-// 		'page_title' 	=> 'Footer Settings',
-// 		'menu_title' 	=> 'Footer Settings',
-// 		'menu_slug'=>'footer_mercadeo',
-// 		'capability' => 'edit_posts',
-// 		'parent_slug' => 'mercadeo_options',
-// 	));
-	
-// }
--- inc/paquetes.php
+++ inc/paquetes.php
@@ -59,4 +59,33 @@ function paquetes_post_type() {
 add_action( 'init', 'paquetes_post_type', 0 );
 
 }
+if ( ! function_exists('paquetes_aditionals_fields') ) {
+	function paquetes_aditionals_fields() {
+	$cmb = new_cmb2_box( array(
+		'id'            => CMB2PREFIX . 'metabox',
+		'title'         => esc_html__( 'Test Metabox', 'cmb2' ),
+		'object_types'  => array( 'paquetes' ), // Post type
+		// 'show_on_cb' => 'yourprefix_show_if_front_page', // function should return a bool value
+		// 'context'    => 'normal',
+		'priority'   => 'high',
+		// 'show_names' => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // true to keep the metabox closed by default
+		// 'classes'    => 'extra-class', // Extra cmb2-wrap classes
+		// 'classes_cb' => 'yourprefix_add_some_classes', // Add classes through a callback.
+	) );	
+	$cmb->add_field( array(
+		'name'       => esc_html__( 'Short Description', 'Avada' ),
+		'desc'       => esc_html__( 'Short description to show in Carousel', 'Avada' ),
+		'id'         => $prefix . 'short_description',
+		'type'       => 'textarea_small',
+		// 'show_on_cb' => 'yourprefix_hide_if_no_cats', // function should return a bool value
+		// 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
+		// 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+		// 'on_front'        => false, // Optionally designate a field to wp-admin only
+		// 'repeatable'      => true,
+		// 'column'          => true, // Display field value in the admin post-listing columns
+	) );
+	}
+	add_action( 'cmb2_admin_init', 'paquetes_aditionals_fields' );
 ?>

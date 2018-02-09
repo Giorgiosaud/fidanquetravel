5329c6845297b753f97d528b98ab893a95b566c5
Jorge Saud <jorgelsaud@gmail.com>
2018-02-08 22:36:26 -0300

    fux acf support

--- functions.php
+++ functions.php
@@ -13,6 +13,37 @@ function avada_lang_setup() {
 add_action( 'after_setup_theme', 'avada_lang_setup' );
 require 'optionspage.php';
 require 'inc/paquetes.php';
+//ACF Package
+add_filter('acf/settings/path', 'my_acf_settings_path');
+function my_acf_settings_path( $path ) {
+ 
+    // update path
+    $path = get_stylesheet_directory() . '/acf/';
+    
+    // return
+    return $path;
+    
+}
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+ 
+function my_acf_settings_dir( $dir ) {
+ 
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/acf/';
+    
+    // return
+    return $dir;
+    
+}
+
+// 3. Hide ACF field group menu item
+// add_filter('acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/acf/acf.php' );
+
 // if( function_exists('acf_add_options_page') ) {
 	
 // 	acf_add_options_page(array(

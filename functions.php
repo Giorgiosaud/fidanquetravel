50bc10137c18bd613139b961a1fe71b7db4f78e5
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 13:21:59 -0300

    no debug sql

--- functions.php
+++ functions.php
@@ -23,13 +23,4 @@ require 'inc/paquetes.php';
 
 
 
-add_action('shutdown', 'sql_logger');
-function sql_logger() {
-    global $wpdb;
-    $log_file = fopen(ABSPATH.'/sql_log.txt', 'a');
-    fwrite($log_file, "//////////////////////////////////////////\n\n" . date("F j, Y, g:i:s a")."\n");
-    foreach($wpdb->queries as $q) {
-        fwrite($log_file, $q[0] . " - ($q[1] s)" . "\n\n");
-    }
-    fclose($log_file);
-}
+

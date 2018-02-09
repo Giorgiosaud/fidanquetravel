fca08e78d58c331055971db817dd5bd2e4d4b140
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 13:19:29 -0300

    debug sql

--- functions.php
+++ functions.php
@@ -20,3 +20,16 @@ if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
 add_image_size( 'carousel', 600, 400 );
 require 'optionspage.php';
 require 'inc/paquetes.php';
+
+
+
+add_action('shutdown', 'sql_logger');
+function sql_logger() {
+    global $wpdb;
+    $log_file = fopen(ABSPATH.'/sql_log.txt', 'a');
+    fwrite($log_file, "//////////////////////////////////////////\n\n" . date("F j, Y, g:i:s a")."\n");
+    foreach($wpdb->queries as $q) {
+        fwrite($log_file, $q[0] . " - ($q[1] s)" . "\n\n");
+    }
+    fclose($log_file);
+}

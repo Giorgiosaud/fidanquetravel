1f888af91c539317c08a241000e57a8afb57866f
Jorge Saud <jorgelsaud@gmail.com>
2018-02-16 11:25:35 -0300

    fix breadcrumb

--- functions.php
+++ functions.php
@@ -2,8 +2,17 @@
 
 function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
-    wp_enqueue_style('customizations',get_stylesheet_directory_uri().'/styles/mercadeostyle.css',array('child-style'),'0.8','all');
-    wp_enqueue_script('customjs',get_stylesheet_directory_uri().'/js/myjs.js',array('jquery'));
+    
+    wp_enqueue_style('daterangepickercss','https://cdnjs.cloudflare.com/ajax/libs/jquery-date-range-picker/0.16.1/daterangepicker.min.css',array('child-style'),'0.16.1','all');
+    wp_enqueue_style('customizations',get_stylesheet_directory_uri().'/styles/mercadeostyle.css',array('daterangepickercss'),'0.8','all');
+    
+
+    wp_enqueue_script('momentjs','https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js',array('jquery'));
+    wp_enqueue_script('momentjses','https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/locale/es-us.js',array('momentjs'));
+    wp_enqueue_script('daterangepicker','https://cdnjs.cloudflare.com/ajax/libs/jquery-date-range-picker/0.16.1/jquery.daterangepicker.min.js',array('momentjses'));
+	wp_enqueue_script('customjs',get_stylesheet_directory_uri().'/js/myjs.js',array('daterangepicker'));
+
+
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 

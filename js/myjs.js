19890f9ae934a4c27e8860ee6d84e17a10364611
Jorge Saud <jorgelsaud@gmail.com>
2018-02-17 17:33:27 -0300

    daterangepicker s1

--- js/myjs.js
+++ js/myjs.js
@@ -16,41 +16,42 @@ jQuery(document).ready(function($) {
 	$('.title.fixh').setMaxHeight();
 	$('.subtitle').setMaxHeight();
 	$(window).on('resize', function(){
-       $('.title.fixh').setMaxHeight();
-       $('.subtitle').setMaxHeight();
-   });	
-
-    $('input.date-depart').dateRangePicker({
-        separator : ' al ',
-        format: 'dddd DD MMM [del] YYYY',
-        getValue: function()
-        {
-            if ($('input.date-depart').val() && $('input.date-return').val() )
-                return $('input.date-depart').val() + ' a ' + $('input.date-return').val();
-            else
-                return '';
-        },
-        setValue: function(s,s1,s2)
-        {
-            $('input.date-depart').val(s1);
-            $('input.date-return').val(s2);
-        }
-    });
-    $('input.date-return').dateRangePicker({
-        separator : ' al ',
-        format: 'dddd DD MMM [del] YYYY',
-        getValue: function()
-        {
-            if ($('input.date-depart').val() && $('input.date-return').val() )
-                return $('input.date-depart').val() + ' a ' + $('input.date-return').val();
-            else
-                return '';
-        },
-        setValue: function(s,s1,s2)
-        {
-            $('input.date-depart').val(s1);
-            $('input.date-return').val(s2);
-        }
-    });
+     $('.title.fixh').setMaxHeight();
+     $('.subtitle').setMaxHeight();
+ });	
+    if($('input.date-depart').length>0){
+        $('input.date-depart').dateRangePicker({
+            separator : ' al ',
+            format: 'dddd DD MMM [del] YYYY',
+            getValue: function()
+            {
+                if ($('input.date-depart').val() && $('input.date-return').val() )
+                    return $('input.date-depart').val() + ' a ' + $('input.date-return').val();
+                else
+                    return '';
+            },
+            setValue: function(s,s1,s2)
+            {
+                $('input.date-depart').val(s1);
+                $('input.date-return').val(s2);
+            }
+        });
+        $('input.date-return').dateRangePicker({
+            separator : ' al ',
+            format: 'dddd DD MMM [del] YYYY',
+            getValue: function()
+            {
+                if ($('input.date-depart').val() && $('input.date-return').val() )
+                    return $('input.date-depart').val() + ' a ' + $('input.date-return').val();
+                else
+                    return '';
+            },
+            setValue: function(s,s1,s2)
+            {
+                $('input.date-depart').val(s1);
+                $('input.date-return').val(s2);
+            }
+        });
+    }
 });
 

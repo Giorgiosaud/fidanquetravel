ff4a3b82f39143712efb67683c30a4196dc3cae5
Jorge Saud <jorgelsaud@gmail.com>
2018-02-17 13:42:12 -0300

    daterangepicker s1

--- js/myjs.js
+++ js/myjs.js
@@ -20,7 +20,23 @@ jQuery(document).ready(function($) {
        $('.subtitle').setMaxHeight();
    });	
 
-    $('input.date-depart,input.date-return').dateRangePicker({
+    $('input.date-depart').dateRangePicker({
+        separator : ' al ',
+        format: 'dddd DD MMM [del] YYYY',
+        getValue: function()
+        {
+            if ($('input.date-depart').val() && $('input.date-return').val() )
+                return $('input.date-depart').val() + ' a ' + $('input.date-return').val();
+            else
+                return '';
+        },
+        setValue: function(s,s1,s2)
+        {
+            $('input.date-depart').val(s1);
+            $('input.date-return').val(s2);
+        }
+    });
+    $('input.date-return').dateRangePicker({
         separator : ' al ',
         format: 'dddd DD MMM [del] YYYY',
         getValue: function()

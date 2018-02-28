2849b9652505234c21f391391d63b1d30f791a67
Jorge Saud <jorgelsaud@gmail.com>
2018-02-28 17:11:43 -0300

    reset child theme

--- functions.php
+++ functions.php
@@ -2,30 +2,40 @@
 
 function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
-    
-    wp_enqueue_style('daterangepickercss','https://cdnjs.cloudflare.com/ajax/libs/jquery-date-range-picker/0.16.1/daterangepicker.min.css',array('child-style'),'0.16.1','all');
-    wp_enqueue_style('customizations',get_stylesheet_directory_uri().'/styles/mercadeostyle.css',array('daterangepickercss'),'0.8','all');
-    
-
-    wp_enqueue_script('momentjs','https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js',array('jquery'));
-    wp_enqueue_script('momentjses','https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/locale/es-us.js',array('momentjs'));
-    wp_enqueue_script('daterangepicker',
-    	get_stylesheet_directory_uri().'/js/daterangepicker.js',
-    	// 'https://cdnjs.cloudflare.com/ajax/libs/jquery-date-range-picker/0.16.1/jquery.daterangepicker.min.js',
-    	array('momentjses'));
-	wp_enqueue_script('customjs',get_stylesheet_directory_uri().'/js/myjs.js',array('daterangepicker'));
-
-
+    wp_enqueue_style('customizations',get_stylesheet_directory_uri().'/styles/mercadeostyle.css',array(''),'0.8','all');
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 function avada_lang_setup() {
-	$lang = get_stylesheet_directory() . '/languages';
-	load_child_theme_textdomain( 'Avada', $lang );
+    $lang = get_stylesheet_directory() . '/languages';
+    load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
-require 'optionspage.php';
-require 'inc/paquetes.php';
+
+// function theme_enqueue_styles() {
+//     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
+    
+    // wp_enqueue_style('daterangepickercss','https://cdnjs.cloudflare.com/ajax/libs/jquery-date-range-picker/0.16.1/daterangepicker.min.css',array('child-style'),'0.16.1','all');
+    // wp_enqueue_style('customizations',get_stylesheet_directory_uri().'/styles/mercadeostyle.css',array('daterangepickercss'),'0.8','all');
+    // wp_enqueue_script('momentjs','https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js',array('jquery'));
+    // wp_enqueue_script('momentjses','https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/locale/es-us.js',array('momentjs'));
+    // wp_enqueue_script('daterangepicker',
+    	// get_stylesheet_directory_uri().'/js/daterangepicker.js',
+    	// 'https://cdnjs.cloudflare.com/ajax/libs/jquery-date-range-picker/0.16.1/jquery.daterangepicker.min.js',
+    	// array('momentjses'));
+	// wp_enqueue_script('customjs',get_stylesheet_directory_uri().'/js/myjs.js',array('daterangepicker'));
+
+
+// }
+// add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+// function avada_lang_setup() {
+// 	$lang = get_stylesheet_directory() . '/languages';
+// 	load_child_theme_textdomain( 'Avada', $lang );
+// }
+// add_action( 'after_setup_theme', 'avada_lang_setup' );
+// require 'optionspage.php';
+// require 'inc/paquetes.php';
 
 
 

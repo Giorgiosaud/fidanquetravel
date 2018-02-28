6ea91a83446042d73fbb9394fba2424c86c27d67
Jorge Saud <jorgelsaud@gmail.com>
2018-02-28 18:16:16 -0300

    add eot ttf support to media

--- functions.php
+++ functions.php
@@ -11,15 +11,17 @@ function avada_lang_setup() {
     load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
-add_filter( 'upload_mimes', 'my_myme_types', 1, 1 );
+add_filter('upload_mimes', 'my_myme_types');
+
 function my_myme_types( $mime_types ) {
-  $mime_types['ttf'] = 'application/font-ttf';     // Adding .svg extension
-  $mime_types['eot'] = 'application/vnd.ms-fontobject'; // Adding .json extension
-  return $mime_types;
+
+    $mime_types['ttf'] = 'application/x-font-ttf';
+    $mime_types['eot'] = 'application/vnd.ms-fontobject';
+    return $mime_types;
 }
 // function theme_enqueue_styles() {
 //     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
-    
+
     // wp_enqueue_style('daterangepickercss','https://cdnjs.cloudflare.com/ajax/libs/jquery-date-range-picker/0.16.1/daterangepicker.min.css',array('child-style'),'0.16.1','all');
     // wp_enqueue_style('customizations',get_stylesheet_directory_uri().'/styles/mercadeostyle.css',array('daterangepickercss'),'0.8','all');
     // wp_enqueue_script('momentjs','https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js',array('jquery'));
